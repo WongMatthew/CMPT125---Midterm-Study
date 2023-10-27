dfac80085c5f6266efa16da9e9bb5be8f712766b
Emily <eha80@sfu.ca>
2023-10-27 12:19:12 -0700

    haha
    updated names for pdfs
    added folder for searching algs, with linear and binary
    added content to  linear searching

---
+++ Sorting Algorithms/Searching Algorithms/Linear.c
@@ -0,0 +1,45 @@
+// Notes: Week 5 (2)
+
+// ============================================================================
+
+/* 
+Linear Search (v1: Boolean)
+
+Main Idea:
+- Go through array
+- Upon finding match with key, return true.
+- If it gets to the end of the array and it does not find a match, return false.
+
+**
+
+Linear Search (v2: Index)
+
+Main Idea:
+- Go through array
+- Upon finding match with key, return index.
+- If it gets to the end of the array and it does not find a match, return -1
+*/
+// ============================================================================
+/*
+Time Complexity:
+Best case: O(I)
+- First array item matches with the key (return array[0])
+Worse case: O(N)
+- Last array matches with the key
+- No item matches with the key
+*/
+// ============================================================================
+/*
+Notes:
+- Linear search is slower than me trying to grasp the concept of coding
+- It is slow because it is looking at each data once
+*/
+// ============================================================================
+/* Pseudocode
+
+for i = 0 to N-1 {
+    if array[i] == key, return i
+}
+return -1;
+
+*/
