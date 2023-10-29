9bdafbbf6e3c5d58ae53af39b2cd0e11cb3819bd
Emily <eha80@sfu.ca>
2023-10-28 17:58:44 -0700

    updated structs.c

---
+++ structs.c
@@ -0,0 +1,10 @@
+// Struct (structure) - Collection of related members  ("variables")
+// Can list different data types listed under one name in a block of memory
+
+//================================================================================
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    return 0;
+}
