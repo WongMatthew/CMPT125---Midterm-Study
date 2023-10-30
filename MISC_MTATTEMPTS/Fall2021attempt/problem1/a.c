810eb6c3a5b634f89b6a5a1979f7cdca8a0bbfb3
Emily <eha80@sfu.ca>
2023-10-30 16:18:10 -0700

    a

--- MISC_MTATTEMPTS/Fall2021attempt/problem1/a.c
+++ MISC_MTATTEMPTS/Fall2021attempt/problem1/a.c
@@ -4,16 +4,15 @@ enum numbers {ZERO, ONE, TWO, THREE};
 
 void foo(int* x, int y){
     int z = 5;
-    *x = z;
-    y = z;
-    x = &y;
+    *x = z; // x = 5
+    y = z;  // y = 5
+    x = &y; //x = []
 }
 
 int main(){
-    int a = ONE, b = TWO;
-    foo(&a, b);
+    int a = ONE, b = TWO;   // one = 1, two = 2
+    foo(&a, b); // 
     printf("a = %d, b = %d", a, b);
     return 0;
 }
 
-// The function will return 2, 1
---
+++ MISC_MTATTEMPTS/Fall2021attempt/problem2/a.c
@@ -0,0 +1,14 @@
+/*
+first time will search for middle item.
+
+2   4   6   8   10  [12]  14  15  18  90  100
+
+2   4   [6]   8   10 
+
+8   [10]
+
+[8]   10
+
+4 comparisons
+
+*/
